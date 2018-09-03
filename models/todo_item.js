170fede803765ed7698436693d04e00f0fdca7a7
BC6 <BC6>
2018-09-03 14:22:44 +0300

    pushing

---
+++ models/todo_item.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Define collection and schema for item To-Do-List
+
+let todo_item = new Schema(
+    {
+        task: {
+        type: String,
+        required: [ true, 'Укажите задачу' ]
+    },
+        isActive: {type: Boolean, default: false}
+    },
+        { timestamps: { createdAt: 'created_at' }}
+    );
+
+let ToDo = mongoose.model('ToDo', todo_item);
+module.exports = ToDo;
