7d0e2e150c3b9410c63f2cd2a46ee59369d9a23d
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-09-04 20:17:27 +0300

    home update

---
+++ client/src/reducers/tasksReducer.js
@@ -0,0 +1,19 @@
+export default function inputsArr (state = [], action) {
+    switch (action.type) {
+        case "LOAD_TASKS":
+            return action.payload;
+        case "ADD_TASK":
+            return [...state, action.payload];
+        case "DELETE_TASK":
+            return state.filter(el => el._id !== action.id);
+        case "EDIT_TASK":
+            const task = action.input;
+            const id = action.id;
+            return state.map(el => el._id === id ? {...el, task} : el);
+        case "IS_ACTIVE":
+            return state.map(el => (el._id === action.id) ? {...el, isActive: !el.isActive} :  {...el, isActive: false});
+
+        default:
+            return state
+    }
+}
